cb78539dcd5e5bda465f89bdebefab93780dfd80
JulOuellet <julien.ouellet@live.ca>
2024-12-17 22:47:25 -0500

    list networks

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use network_manager::NetworkManager;
+use std::error::Error;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    // Establish a connection to the NetworkManager
+    let network_manager = NetworkManager::new();
+
+    let devices = network_manager.get_devices()?;
+    let mut wifi_networks = Vec::new();
+
+    for device in devices {
+        if let Some(wifi) = device.as_wifi_device() {
+            let access_points = wifi.get_access_points();
+            wifi_networks.extend(access_points);
+        }
+    }
+
+    println!("Available Wi-Fi Networks:");
+    for ap in wifi_networks {
+        ap.iter().for_each(|item| println!("{}", item.ssid().as_str().unwrap()));    
+    }
+
+    Ok(())
 }
+

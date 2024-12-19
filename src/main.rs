1d286f496c786a727ead1ca406f32a6cdf12561d
JulOuellet <julien.ouellet@live.ca>
2024-12-18 22:36:40 -0500

    update err handling

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,6 @@ use std::error::Error;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    // Establish a connection to the NetworkManager
     let network_manager = NetworkManager::new();
 
     let devices = network_manager.get_devices()?;
@@ -11,14 +10,18 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     for device in devices {
         if let Some(wifi) = device.as_wifi_device() {
-            let access_points = wifi.get_access_points();
+            let access_points = wifi.get_access_points()?;
             wifi_networks.extend(access_points);
         }
     }
 
     println!("Available Wi-Fi Networks:");
     for ap in wifi_networks {
-        ap.iter().for_each(|item| println!("{}", item.ssid().as_str().unwrap()));    
+        let ssid_slice = ap.ssid();
+        match ssid_slice.as_str() {
+            Ok(ssid) => println!("{}", ssid),
+            Err(_) => println!("(Hidden Network)"),
+        }
     }
 
     Ok(())

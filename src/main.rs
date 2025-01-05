e6c11ffb17e17a83cc7db4cf6e40abb70dbe9621
JulOuellet <julien.ouellet@live.ca>
2025-01-04 22:25:04 -0500

    clean main module

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,10 @@
-use std::{
-    io::{stdout, Result}, 
-    time::Duration
-};
+use std::io::Result;
 
-use crossterm::{
-    event::{self, Event}, 
-    terminal::{
-        disable_raw_mode, 
-        enable_raw_mode, 
-        EnterAlternateScreen, 
-        LeaveAlternateScreen
-    }, 
-    ExecutableCommand
-};
-
-use ratatui::{
-    backend::CrosstermBackend, 
-    Terminal
-};
-
-use wi_tui::{app::App, ui::draw_ui};
+use wi_tui::app::App;
 
 fn main() -> Result<()> {
-
     let mut app = App::new();
-
-    enable_raw_mode()?;
-    stdout().execute(EnterAlternateScreen)?;
-    let backend = CrosstermBackend::new(stdout());
-    let mut terminal = Terminal::new(backend)?;
-    
-    app.refresh_networks();
-
-    while app.running {
-        terminal.draw(|frame| draw_ui(frame, &app))?;
-
-        if event::poll(Duration::from_millis(250))? {
-            app.handle_event()?;
-        }
-    }
-
-    disable_raw_mode()?;
-    stdout().execute(LeaveAlternateScreen)?;
+    app.run()?;
 
     Ok(())
 }
